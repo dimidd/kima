1a216bc4296243d14f3741f0734d9e9b7fdbe46c
Dimid Duchovny <dimidd@gmail.com>
2016-12-09 13:13:27 +0200

    tmp
    Signed-off-by: Dimid Duchovny <dimidd@gmail.com>

--- marc2json.rb
+++ marc2json.rb
@@ -2,6 +2,27 @@ require 'marc'
 require 'yaml'
 require 'json/ext'
 
+def process_rec rec, field2desc
+  res_rec = {}
+  rec.fields.each do |field|
+    desc = field2desc[field.tag]
+    if desc
+      res_rec[desc] ||= []
+      if field.respond_to? :subfields
+        subfields = {}
+        field.subfields.each do |sub|
+          subfields[sub.code] = sub.value
+        end
+        res_rec[desc] << subfields
+      else
+        res_rec[desc] << field.value
+      end
+    end
+  end
+
+  res_rec
+end
+
 if ARGV.size != 2
   puts "usage: marc2json <MARC XML file> <fields YAML file>"
   exit
@@ -9,30 +30,14 @@ end
 
 field2desc = YAML.load_file ARGV[1]
 res = []
-reader = MARC::XMLReader.new(ARGV[0])
+reader = MARC::XMLReader.new(ARGV[0], :external_encoding => "MARC-8")
 begin
   reader.each do |rec|
-    res_rec = {}
-      rec.fields.each do |field|
-        desc = field2desc[field.tag]
-        if desc
-          res_rec[desc] = {}
-          if field.respond_to? :subfields
-            res_rec[desc]['subfields'] = {}
-            field.subfields.each do |sub|
-              res_rec[desc]['subfields'][sub.code] = sub.value
-            end
-          else
-            res_rec[desc] = field.value
-          end
-        end
-      end
-      res << res_rec
+    res_rec = process_rec rec, field2desc
+    res << res_rec if res_rec
   end
 rescue => e
   STDERR.puts e
   STDERR.puts e.backtrace
 end
-final = {}
-final['records'] = res
-puts final.to_json
+puts res.to_json
